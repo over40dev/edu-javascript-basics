1939f189212167f251b2e074bb4345439d7b3914
over40dev <over40dev.com>
2020-03-26 01:50:56 -0230

    17-for-loops

--- src/basics.js
+++ src/basics.js
@@ -1,16 +1,3 @@
-const controlStatements = () => {
-  // 16 - control statements
-  logMessage(16, 'control-statements', 'begin');
-  const endPos = 5;
-  let
-    xPos = 0,
-    enemyPos = 4;
-
-  movePlayer({xPos,endPos,enemyPos});
-
-  logMessage(16, 'control-statements', 'end');
-}
-
 const whileLoops = () => {
   // 15-while-loops
   logMessage(15, 'while-loops', 'begin');
@@ -30,13 +17,45 @@ const whileLoops = () => {
   logMessage(15, 'while-loops', 'end');
 }
 
-export default controlStatements;
+const controlStatements = () => {
+  // 16 - control statements
+  logMessage(16, 'control-statements', 'begin');
+  const endPos = 5;
+  let
+    xPos = 0,
+    enemyPos = 4;
+
+  movePlayer({ xPos, endPos, enemyPos });
+
+  logMessage(16, 'control-statements', 'end');
+}
+
+const forLoops = () => {
+  logMessage(17, 'for-loops', 'begin');
+  const items = ['Axe', 'Shirt', 'Knife'];
+  let finalString = 'In my inventory, I have ';
+
+  // for loop
+  // for (let i = 0; i < items.length; i++) {
+  //   finalString += (i+1 < items.length) ? `${items[i]}, ` : `and ${items[i]}!`;
+  // }
+
+  // [].forEach()
+  items.forEach((item, i) => {
+    finalString += (i+1 < items.length) ? `${items[i]}, ` : `and ${items[i]}!`; 
+  });
+
+  logMessage(17, 'for-loops', finalString);
+  logMessage(17, 'for-loops', 'end');
+}
+
+export default forLoops;
 
 function logMessage(num, section, ext) {
   console.log(`${num}-${section}-${ext}`);
 }
 
-function movePlayer({xPos, endPos, enemyPos}) {
+function movePlayer({ xPos, endPos, enemyPos }) {
   while (xPos < endPos) {
     xPos++;
     if (xPos % 2 === 1) {
